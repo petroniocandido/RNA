5935239b77bf3e0c233180a4bebbbcfab4c3c1ed
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2021-09-04 09:06:18 -0300

    Update FuncoesAtivacao.py

--- FuncoesAtivacao.py
+++ FuncoesAtivacao.py
@@ -1,1 +1,18 @@
+def degrau(x, derivada = False):
+  if not derivada:
+    return np.where(x > 0, 1, 0)
+  else:
+    return np.where(x > 0, 1, 0)
 
+def sigmoide(x, derivada = False):
+  if not derivada:
+    return 1./(1. + np.exp(-x))
+  else:
+    y = sigmoide(x)
+    return y*(1-y)
+
+def relu(x, derivada = False):
+  if not derivada:
+    return np.maximum(x, 0)
+  else:
+    return np.where(np.maximum(x, 0) > 0, 1, 0) 
